1355e089058a0f4611160ce63eb50ef3e5aab44f
Sanket Upadhyay <sanket.upadhyay@infoud.co.in>
2018-07-21 15:18:24 +0530

    Tests updated

--- src/tests/base.spec.ts
+++ src/tests/base.spec.ts
@@ -15,11 +15,56 @@ describe('base', () => {
         expect(base.FunBase).toBeTruthy()
     });
 
-    it('should have variable \'a\' with value 5', () => {
-        expect(this.baseObj.a).toEqual(5)
+    /* Types */
+    
+    it('should have variable \'varNum\' of type number', () => {
+        expect(typeof this.baseObj.varNum).toEqual('number')
     });
 
-    it('should have variable \'b\' with value \'test\'', () => {
-        expect(this.baseObj.b).toEqual('test')
+    it('should have variable \'varStr\' of type string', () => {
+        expect(typeof this.baseObj.varStr).toEqual('string')
     });
+
+    it('should have variable \'varBool\' of type boolean', () => {
+        expect(typeof this.baseObj.varBool).toEqual('boolean')
+    });
+
+    it('should have variable \'varArr\' of type array', () => {
+        expect(this.baseObj.varArr instanceof Array).toEqual(true)
+    });
+
+    it('should have variable \'varObj\' of type obj', () => {
+        expect(typeof this.baseObj.varObj).toEqual('object')
+    });
+
+    it('should have variable \'varAny\' of type any', () => {
+        expect(typeof this.baseObj.varAny).toEqual('number'||'string'||'boolean'||'object')
+    });
+
+    /* Value Assignment */
+
+    it('should have variable \'varNum\' with value 5', () => {
+        expect(this.baseObj.varNum).toEqual(5)
+    });
+
+    it('should have variable \'varStr\' with value \'test\'', () => {
+        expect(this.baseObj.varStr).toEqual('test')
+    });
+
+    it('should have variable \'varBool\' with value true', () => {
+        expect(this.baseObj.varBool).toEqual(true)
+    });
+
+    it('should have variable \'varArr\' with array []', () => {
+        expect(this.baseObj.varArr).toEqual([])
+    });
+
+    it('should have variable \'varObj\' of type obj', () => {
+        expect(this.baseObj.varObj).toEqual({})
+    });
+
+    it('should have variable \'varAny\' of type any', () => {
+        expect(this.baseObj.varAny).toEqual(123)
+    });
+
 });
